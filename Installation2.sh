645ba9ec739073736097e23a9f0df9cbc51a3814
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-06-08 14:25:53 +0530

    Update Installation2.sh

--- Installation2.sh
+++ Installation2.sh
@@ -1,4 +1,6 @@
 juju add-relation mysql-innodb-cluster:certificates vault:certificates
+
+#neutron-networking
 juju deploy -n 3 --channel 22.09/stable ovn-central
 juju deploy --channel zed/stable --config neutron.yaml neutron-api
 juju deploy --channel zed/stable neutron-api-plugin-ovn
@@ -14,15 +16,21 @@ juju add-relation ovn-chassis:certificates vault:certificates
 juju deploy --channel 8.0/stable mysql-router neutron-api-mysql-router
 juju add-relation neutron-api-mysql-router:db-router mysql-innodb-cluster:db-router
 juju add-relation neutron-api-mysql-router:shared-db neutron-api:shared-db
+
+#keystone
 juju deploy --channel zed/stable keystone
 juju deploy --channel 8.0/stable mysql-router keystone-mysql-router
 juju add-relation keystone-mysql-router:db-router mysql-innodb-cluster:db-router
 juju add-relation keystone-mysql-router:shared-db keystone:shared-db
 juju add-relation keystone:identity-service neutron-api:identity-service
 juju add-relation keystone:certificates vault:certificates
+
+#rabbitmq
 juju deploy --channel 3.9/stable rabbitmq-server
 juju add-relation rabbitmq-server:amqp neutron-api:amqp
 juju add-relation rabbitmq-server:amqp nova-compute:amqp
+
+#Nova cloud controller
 juju deploy --channel zed/stable --config ncc.yaml nova-cloud-controller
 juju deploy --channel 8.0/stable mysql-router ncc-mysql-router
 juju add-relation ncc-mysql-router:db-router mysql-innodb-cluster:db-router
@@ -32,6 +40,7 @@ juju add-relation nova-cloud-controller:amqp rabbitmq-server:amqp
 juju add-relation nova-cloud-controller:neutron-api neutron-api:neutron-api
 juju add-relation nova-cloud-controller:cloud-compute nova-compute:cloud-compute
 juju add-relation nova-cloud-controller:certificates vault:certificates
+
 #placement
 juju deploy --channel zed/stable placement
 juju deploy --channel 8.0/stable mysql-router placement-mysql-router
@@ -40,6 +49,7 @@ juju add-relation placement-mysql-router:shared-db placement:shared-db
 juju add-relation placement:identity-service keystone:identity-service
 juju add-relation placement:placement nova-cloud-controller:placement
 juju add-relation placement:certificates vault:certificates
+
 #openstack-dashboard
 juju deploy --channel zed/stable openstack-dashboard
 juju deploy --channel 8.0/stable mysql-router dashboard-mysql-router
@@ -47,6 +57,7 @@ juju add-relation dashboard-mysql-router:db-router mysql-innodb-cluster:db-route
 juju add-relation dashboard-mysql-router:shared-db openstack-dashboard:shared-db
 juju add-relation openstack-dashboard:identity-service keystone:identity-service
 juju add-relation openstack-dashboard:certificates vault:certificates
+
 #glance
 juju deploy --channel zed/stable glance
 juju deploy --channel 8.0/stable mysql-router glance-mysql-router
@@ -56,11 +67,13 @@ juju add-relation glance:image-service nova-cloud-controller:image-service
 juju add-relation glance:image-service nova-compute:image-service
 juju add-relation glance:identity-service keystone:identity-service
 juju add-relation glance:certificates vault:certificates
+
 #ceph-moonitor
 juju deploy -n 3 --channel quincy/stable --config ceph-mon.yaml ceph-mon
 juju add-relation ceph-mon:osd ceph-osd:mon
 juju add-relation ceph-mon:client nova-compute:ceph
 juju add-relation ceph-mon:client glance:ceph
+
 #cinder
 juju deploy --channel zed/stable --config cinder.yaml cinder
 juju deploy --channel 8.0/stable mysql-router cinder-mysql-router
@@ -74,6 +87,7 @@ juju add-relation cinder:certificates vault:certificates
 juju deploy --channel zed/stable cinder-cephjuju add-relation cinder-ceph:storage-backend cinder:storage-backend
 juju add-relation cinder-ceph:ceph ceph-mon:client
 juju add-relation cinder-ceph:ceph-access nova-compute:ceph-access
+
 #ceph-radosgw
 juju deploy --channel quincy/stable ceph-radosgw
 juju add-relation ceph-radosgw:mon ceph-mon:radosgw
